6cbe0e428268fbececb7b1e04f9bf25b084a9e8e
jnk254 <jndura57@gmail.com>
2020-03-27 23:52:08 -0400

    A list of services added

---
+++ app/src/main/java/com/example/bsacco/Task.java
@@ -0,0 +1,60 @@
+package com.example.bsacco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Task extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+
+        ListView listview = (ListView) findViewById(R.id.services);
+
+        String [] services = new String[]{"Deposit","Withdrawal","Transfer Money","Borrow Loan","Payments","Savings"};
+
+        ArrayAdapter adapter = new ArrayAdapter<>(this,android.R.layout.activity_list_item, android.R.id.text1, services);
+                listview.setAdapter(adapter);
+         listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+             @Override
+             public void onItemClick(AdapterView<?> adapterView, View view, int service, long l) {
+                if(service==0){
+                    Intent intent = new Intent(Task.this,Deposit.class);
+                    startActivity(intent);
+                }
+                if(service==1){
+                    Intent intent = new Intent(Task.this,Withdrawal.class);
+                    startActivity(intent);
+                }
+                if(service==2){
+                    Intent intent = new Intent(Task.this,Transfer.class);
+                    startActivity(intent);
+                }
+                if(service==3){
+                    Intent intent = new Intent(Task.this,Loan.class);
+                    startActivity(intent);
+                }
+                if(service==4){
+                    Intent intent = new Intent(Task.this,Payments.class);
+                    startActivity(intent);
+                }
+                if(service==5){
+                    Intent intent = new Intent(Task.this,Savings.class);
+                    startActivity(intent);
+                }
+
+            }
+
+        });
+    }
+}
